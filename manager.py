19edc1bdc06bd2129e5abdd11ae01098bca79822
stumash <stuart.mashaal@gmail.com>
2019-03-26 17:36:40 -0400

    can run commands in containers

--- manager.py
+++ manager.py
@@ -3,5 +3,11 @@ import subprocess
 def system(command_string):
     subprocess.call(["/usr/bin/env", "bash", "-c", command_string])
 
+def run_in_container(command_string):
+    system("docker run ubuntu " + command_string)
+
 if __name__ == "__main__":
-    system("echo hello, world")
+    run_in_container("echo hello, world")
+
+    print("\n"+"-"*5+"cleaning up exited containers"+"-"*5)
+    system("yes | docker container prune")
